d35cb34b5027003022de190dbdddbc1dd8824eaa
Nicholas Capel <nicholas.rj.capel@gmail.com>
2020-04-23 22:00:29 +0800

    Gain trajectory information

--- examples/pendulum_swing_up_pilco.py
+++ examples/pendulum_swing_up_pilco.py
@@ -1,5 +1,7 @@
 import numpy as np
 import gym
+
+from examples.utils import predict_trajectory_wrapper
 from pilco.models import PILCO
 from pilco.controllers import RbfController, LinearController, CombinedController
 from pilco.controller_utils import LQR
@@ -107,13 +109,11 @@ with tf.Session() as sess:
         pilco.optimize_policy(maxiter=maxiter, restarts=2)
         print(f'S:{controller.S}, zeta:{controller.zeta}')
         X_new, Y_new = rollout(env, pilco, timesteps=T_sim, verbose=True, SUBS=SUBS, render=True)
-
         # Since we had decide on the various parameters of the reward function
         # we might want to verify that it behaves as expected by inspection
-        # cur_rew = 0
-        # for t in range(0,len(X_new)):
-        #     cur_rew += reward_wrapper(R, X_new[t, 0:state_dim, None].transpose(), 0.0001 * np.eye(state_dim))[0]
-        # print('On this episode reward was ', cur_rew)
+        cur_rew = []
+        for t in range(0,len(X_new)):
+            cur_rew.append(predict_trajectory_wrapper(pilco, X_new[t, 0:state_dim, None].transpose(), 0.0001 * np.eye(state_dim), T))
 
         # Update dataset
         X = np.vstack((X, X_new));
