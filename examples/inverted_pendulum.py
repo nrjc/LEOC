00bc0a271bb5a8b0fc79c2d803af4a5beb7bad07
Funaizhang <naifuzhang@gmail.com>
2020-06-27 01:03:37 +0800

    Reset original PILCO examples

--- examples/inverted_pendulum.py
+++ examples/inverted_pendulum.py
@@ -17,16 +17,6 @@ for i in range(1,5):
     X_, Y_, _, _ = rollout(env=env, pilco=None, random=True,  timesteps=40, render=True)
     X = np.vstack((X, X_))
     Y = np.vstack((Y, Y_))
-np.random.seed(0)
-
-# with tf.Session(graph=tf.Graph()) as sess:
-env = gym.make('InvertedPendulum-v2')
-# Initial random rollouts to generate a dataset
-X,Y = rollout(env=env, pilco=None, random=True, timesteps=40)
-for i in range(1,3):
-    X_, Y_ = rollout(env=env, pilco=None, random=True,  timesteps=40)
-    X = np.vstack((X, X_))
-    Y = np.vstack((Y, Y_))
 
 
 state_dim = Y.shape[1]
