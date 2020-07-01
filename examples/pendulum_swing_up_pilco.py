0a8cea1892f33605092f9f4b83f605620e97fe72
Funaizhang <naifuzhang@gmail.com>
2020-07-01 16:28:35 +0800

    Implement testing code for linear controller

--- examples/pendulum_swing_up_pilco.py
+++ examples/pendulum_swing_up_pilco.py
@@ -33,16 +33,20 @@ class myPendulum():
     def step(self, action):
         return self.env.step(action)
 
-    def reset(self):
+    def reset(self, up=False):
         high = np.array([np.pi, 1])
         self.env.state = np.random.uniform(low=-high, high=high)
         self.env.state = np.random.uniform(low=0, high=0.01 * high)  # only difference
-        self.env.state[0] += -np.pi
+        if not up:
+            self.env.state[0] += -np.pi
         self.env.last_u = None
         return self.env._get_obs()
 
     def render(self):
         self.env.render()
+
+    def close(self):
+        self.env.close()
 
     def control(self):
         # m := mass of pendulum
@@ -51,16 +55,16 @@ class myPendulum():
         b = 0
         g = self.env.g
         m = self.env.m
-        l = self.env.l
-        I = 1 / 12 * m * (l ** 2)
-        p = 1 / 4 * m * (l ** 2) + I
+        l = self.env.l / 2
+        I = 1 / 3 * m * (l ** 2)
+        p = m * (l ** 2) + I
 
         # using x to approximate sin(x)
         A = np.array([[0, 1],
-                      [1 / 2 * m * l * g / p, -b / p]])
+                      [m * l * g / p, -b / p]])
 
         B = np.array([[0],
-                      [1 / p]])
+                      [-1 / p]])
 
         C = np.array([[1, 0]])
 
@@ -77,6 +81,7 @@ class myPendulum():
 
 if __name__ == '__main__':
     # Define params
+    test_linear_control = True
     SUBS = 3
     bf = 60
     maxiter = 50
@@ -98,55 +103,69 @@ if __name__ == '__main__':
 
     state_dim = 3
     control_dim = 1
-    # controller = LinearController(state_dim=state_dim, control_dim=control_dim, W=-W_matrix, max_action=1)
+    controller_linear = LinearController(state_dim=state_dim, control_dim=control_dim, W=W_matrix, max_action=1)
     controller = CombinedController(state_dim=state_dim, control_dim=control_dim, num_basis_functions=bf,
-                                    controller_location=target, W=-W_matrix, max_action=2.0)
+                                    controller_location=target, W=W_matrix, max_action=2.0)
     R = ExponentialReward(state_dim=state_dim, t=target, W=weights)
     # c_param = L2HarmonicPenalization([controller.get_S()], 0.0001)
     # R = CombinedRewards(state_dim, [R, c_param])
 
-    # Initial random rollouts to generate a dataset
-    X, Y, _, _ = rollout(env, None, timesteps=T, random=True, SUBS=SUBS, render=True, verbose=False)
-    for i in range(1, J):
-        X_, Y_, _, _ = rollout(env, None, timesteps=T, random=True, SUBS=SUBS, render=True, verbose=False)
-        X = np.vstack((X, X_))
-        Y = np.vstack((Y, Y_))
-    pilco = PILCO((X, Y), controller=controller, horizon=T, reward=R, m_init=m_init, S_init=S_init)
+    if not test_linear_control:
+        # Initial random rollouts to generate a dataset
+        X, Y, _, _ = rollout(env, None, timesteps=T, random=True, SUBS=SUBS, render=True, verbose=False)
+        for i in range(1, J):
+            X_, Y_, _, _ = rollout(env, None, timesteps=T, random=True, SUBS=SUBS, render=True, verbose=False)
+            X = np.vstack((X, X_))
+            Y = np.vstack((Y, Y_))
+        pilco = PILCO((X, Y), controller=controller, horizon=T, reward=R, m_init=m_init, S_init=S_init)
 
-    # for numerical stability, we can set the likelihood variance parameters of the GP models
-    for model in pilco.mgpr.models:
-        model.likelihood.variance.assign(0.001)
-        set_trainable(model.likelihood.variance, False)
-    axis_values = np.zeros((N, state_dim))
-    r_new = np.zeros((T, 1))
-    for rollouts in range(N):
-        print("**** ITERATION no", rollouts, " ****")
-        pilco.optimize_models(maxiter=maxiter, restarts=2)
-        pilco.optimize_policy(maxiter=maxiter, restarts=2)
-        s_val = pilco.get_controller().get_S()
-        axis_values[rollouts, :] = s_val.numpy()
-        X_new, Y_new, _, _ = rollout(env, pilco, timesteps=T_sim, verbose=False, SUBS=SUBS, render=True)
+        # for numerical stability, we can set the likelihood variance parameters of the GP models
+        for model in pilco.mgpr.models:
+            model.likelihood.variance.assign(0.001)
+            set_trainable(model.likelihood.variance, False)
+        axis_values = np.zeros((N, state_dim))
+        r_new = np.zeros((T, 1))
+        for rollouts in range(N):
+            print("**** ITERATION no", rollouts, " ****")
+            pilco.optimize_models(maxiter=maxiter, restarts=2)
+            pilco.optimize_policy(maxiter=maxiter, restarts=2)
+            s_val = pilco.get_controller().get_S()
+            axis_values[rollouts, :] = s_val.numpy()
+            X_new, Y_new, _, _ = rollout(env, pilco, timesteps=T_sim, verbose=False, SUBS=SUBS, render=True)
 
-        # Since we had decide on the various parameters of the reward function
-        # we might want to verify that it behaves as expected by inspection
-        for i in range(len(X_new)):
-            r_new[:, 0] = R.compute_reward(X_new[i, None, :-1], 0.001 * np.eye(state_dim))[0]
-        total_r = sum(r_new)
-        _, _, r, intermediary_dict = pilco.predict_and_obtain_intermediates(X_new[0, None, :-1], 0.001 * S_init, T)
-        print("Total ", total_r, " Predicted: ", r)
-        plt.figure(3)
-        for c_dim in range(state_dim):
-            plt.plot(axis_values[:rollouts, c_dim])
-        plt.pause(0.01)
-        # Plotting internal states of pilco variables
-        intermediate_mean, intermediate_var, intermediate_reward = zip(*intermediary_dict)
-        intermediate_var = [x.diagonal() for x in intermediate_var]
-        intermediate_mean = [x[0] for x in intermediate_mean]
-        plot_single_rollout_cycle(intermediate_mean, intermediate_var, [X_new], None, None, state_dim,
-                                  control_dim, T, rollouts + 1)
+            # Since we had decide on the various parameters of the reward function
+            # we might want to verify that it behaves as expected by inspection
+            for i in range(len(X_new)):
+                r_new[:, 0] = R.compute_reward(X_new[i, None, :-1], 0.001 * np.eye(state_dim))[0]
+            total_r = sum(r_new)
+            _, _, r, intermediary_dict = pilco.predict_and_obtain_intermediates(X_new[0, None, :-1], 0.001 * S_init, T)
+            print("Total ", total_r, " Predicted: ", r)
+            plt.figure(3)
+            for c_dim in range(state_dim):
+                plt.plot(axis_values[:rollouts, c_dim])
+            plt.pause(0.01)
+            # Plotting internal states of pilco variables
+            intermediate_mean, intermediate_var, intermediate_reward = zip(*intermediary_dict)
+            intermediate_var = [x.diagonal() for x in intermediate_var]
+            intermediate_mean = [x[0] for x in intermediate_mean]
+            plot_single_rollout_cycle(intermediate_mean, intermediate_var, [X_new], None, None, state_dim,
+                                      control_dim, T, rollouts + 1)
 
-        # Update dataset
-        X = np.vstack((X, X_new))
-        Y = np.vstack((Y, Y_new))
-        pilco.mgpr.set_data((X, Y))
-    plt.show()
+            # Update dataset
+            X = np.vstack((X, X_new))
+            Y = np.vstack((Y, Y_new))
+            pilco.mgpr.set_data((X, Y))
+        plt.show()
+
+    else:
+        states = env.reset(up=test_linear_control)
+        for i in range(100):
+            env.render()
+            action = controller_linear.compute_action(tf.reshape(tf.convert_to_tensor(states), (1, -1)),
+                                               tf.zeros([state_dim, state_dim], dtype=tf.dtypes.float64),
+                                               squash=False)[0]
+            action = action[0, :].numpy()  # + random.normalvariate(0, 0.1)
+            states, _, _, _ = env.step(action)
+            print(f'Step: {i}, action: {action}')
+
+    env.close()
