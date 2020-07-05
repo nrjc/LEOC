b381f564bba84c720f2ef18579c6b1924d997598
Funaizhang <naifuzhang@gmail.com>
2020-07-05 22:42:36 +0800

    WIP mountain_car combined_controller

--- examples/mountain_car_pilco.py
+++ examples/mountain_car_pilco.py
@@ -6,7 +6,7 @@ from pilco.models import PILCO
 from pilco.controllers import RbfController, LinearController, CombinedController
 from pilco.plotting_utils import plot_single_rollout_cycle
 from pilco.rewards import ExponentialReward
-from examples.envs.mountain_car_env import Continuous_MountainCarEnv
+from examples.envs.mountain_car_env import Continuous_MountainCarEnv as MountainCarEnv
 import tensorflow as tf
 from utils import rollout, policy
 from matplotlib import pyplot as plt
@@ -16,7 +16,7 @@ np.random.seed(0)
 
 class myMountainCar():
     def __init__(self):
-        self.env = Continuous_MountainCarEnv()
+        self.env = MountainCarEnv()
         self.action_space = self.env.action_space
         self.observation_space = self.env.observation_space
 
@@ -38,9 +38,9 @@ class myMountainCar():
         self.env.close()
 
     def control(self):
-        # m := mass of cart
-        # b := coefficient of friction of pendulum
-        b = 0 # assume frictionless system
+        # m := mass of car
+        # b := coefficient of friction of mountain. 'MountainCarEnv' env is frictionless
+        b = 0
         g = self.env.gravity
         m = self.env.masscart
 
@@ -58,10 +58,23 @@ class myMountainCar():
 
 if __name__ == '__main__':
     # Define params
-    test_linear_control = True
-    SUBS = 5
-    T = 25
+    test_linear_control = False
+    SUBS = 3
+    bf = 60
+    maxiter = 50
     max_action = 50.0
+    T = 40
+    T_sim = T
+    J = 5
+    N = 8
+    restarts = 2
+
+    # Need to double check init values
+    # States := [x, x_dot]
+    target = np.array([0.0, 0.0])
+    weights = np.diag([2.0, 0.3])
+    m_init = np.reshape([-np.pi, 0.0], (1, 2))
+    S_init = np.diag([0.01, 0.01])
 
     env = myMountainCar()
     A, B, C = env.control()
@@ -70,13 +83,65 @@ if __name__ == '__main__':
     # Set up objects and variables
     state_dim = 2
     control_dim = 1
-    controller = LinearController(state_dim=state_dim, control_dim=control_dim, W=W_matrix, max_action=max_action)
+    controller_linear = LinearController(state_dim=state_dim, control_dim=control_dim, W=W_matrix,
+                                         max_action=max_action)
+    controller = CombinedController(state_dim=state_dim, control_dim=control_dim, num_basis_functions=bf,
+                                    controller_location=target, W=W_matrix, max_action=max_action)
+    R = ExponentialReward(state_dim=state_dim, t=target, W=weights)
 
-    if test_linear_control:
+    if not test_linear_control:
+        # Initial random rollouts to generate a dataset
+        X, Y, _, _ = rollout(env=env, pilco=None, timesteps=T, random=True, SUBS=SUBS, render=True, verbose=False)
+        for i in range(1, J):
+            X_, Y_, _, _ = rollout(env=env, pilco=None, timesteps=T, random=True, SUBS=SUBS, render=True, verbose=False)
+            X = np.vstack((X, X_))
+            Y = np.vstack((Y, Y_))
+        pilco = PILCO((X, Y), controller=controller, horizon=T, reward=R, m_init=m_init, S_init=S_init)
+
+        # for numerical stability, we can set the likelihood variance parameters of the GP models
+        for model in pilco.mgpr.models:
+            model.likelihood.variance.assign(0.001)
+            set_trainable(model.likelihood.variance, False)
+        axis_values = np.zeros((N, state_dim))
+        r_new = np.zeros((T, 1))
+
+        for rollouts in range(N):
+            print("**** ITERATION no", rollouts, " ****")
+            pilco.optimize_models(maxiter=maxiter, restarts=2)
+            pilco.optimize_policy(maxiter=maxiter, restarts=2)
+            s_val = pilco.get_controller().get_S()
+            axis_values[rollouts, :] = s_val.numpy()
+            X_new, Y_new, _, _ = rollout(env, pilco, timesteps=T_sim, verbose=False, SUBS=SUBS, render=True)
+
+            # Since we had decide on the various parameters of the reward function
+            # we might want to verify that it behaves as expected by inspection
+            for i in range(len(X_new)):
+                r_new[:, 0] = R.compute_reward(X_new[i, None, :-1], 0.001 * np.eye(state_dim))[0]
+            total_r = sum(r_new)
+            _, _, r, intermediary_dict = pilco.predict_and_obtain_intermediates(X_new[0, None, :-1], 0.001 * S_init, T)
+            print("Total ", total_r, " Predicted: ", r)
+            plt.figure(3)
+            for c_dim in range(state_dim):
+                plt.plot(axis_values[:rollouts, c_dim])
+            plt.pause(0.01)
+            # Plotting internal states of pilco variables
+            intermediate_mean, intermediate_var, intermediate_reward = zip(*intermediary_dict)
+            intermediate_var = [x.diagonal() for x in intermediate_var]
+            intermediate_mean = [x[0] for x in intermediate_mean]
+            plot_single_rollout_cycle(intermediate_mean, intermediate_var, [X_new], None, None, state_dim,
+                                      control_dim, T, rollouts + 1, env='mountain car')
+
+            # Update dataset
+            X = np.vstack((X, X_new))
+            Y = np.vstack((Y, Y_new))
+            pilco.mgpr.set_data((X, Y))
+        plt.show()
+
+    else:
         states = env.reset(up=test_linear_control)
         for i in range(200):
             env.render()
-            action = controller.compute_action(tf.reshape(tf.convert_to_tensor(states), (1, -1)),
+            action = controller_linear.compute_action(tf.reshape(tf.convert_to_tensor(states), (1, -1)),
                                                tf.zeros([state_dim, state_dim], dtype=tf.dtypes.float64),
                                                squash=False)[0]
             action = action[0, :].numpy()
