a6c942d028fc3eb1240dac43741d43d228405362
Funaizhang <naifuzhang@gmail.com>
2020-08-03 23:12:27 +0800

    Stability experiment results

--- pilco/plotting_utils.py
+++ pilco/plotting_utils.py
@@ -5,6 +5,11 @@ import numpy as np
 import seaborn as sns
 from gym import logger
 import pandas as pd
+import csv
+from numpy import genfromtxt
+import pandas as pd
+from matplotlib.ticker import PercentFormatter
+
 
 
 def plot_single_rollout_cycle(state_mean: List[np.ndarray], state_var: List[np.ndarray],
@@ -19,17 +24,20 @@ def plot_single_rollout_cycle(state_mean: List[np.ndarray], state_var: List[np.n
         state_var: T x N. A list (length T) of numpy array of dimension (N), denoting the variance at each time step.
         rollout: S x T x N. A list (length S) of list (length T) of numpy array of dimension (N), denoting the observed parameter state.
         rollout_action: T x M. A list (length T) of numpy array of dimension (M), denoting the actions taken by rollout controller.
-        all_rewards: 1 x up to N. A list (length up to N) of tensors, denoting the reward at the end of all previous rollouts.
+        all_rewards: 1 x N. A list (length N) of tensors, denoting the reward at the end of all previous rollouts.
+        all_S: N x lambda_dim.  A list (length N) of tensors of dimension lambda_dim, denoting the lambdas of the n-ellipsoid at end of all previous rollouts.
+        rollout_ratio: 1 x T. A list (length up to T) of tensors, denoting the actual ratio for linear controller.
         internal_state_dim_num: size of internal state dimensions
         action_dim_num: size of action dimensions
         time_steps: time steps
         rollout_num: represents the number of rollouts
         env: the experiment environment, string
+        write_to_csv: boolean
     Returns:
         None
 
     """
-    # TODO: Add titles
+
     save_data = {}
     total_graphs = internal_state_dim_num + 3
     width = 3
@@ -140,22 +148,40 @@ def plot_single_rollout_cycle(state_mean: List[np.ndarray], state_var: List[np.n
         print(f'ITERATION {rollout_num} saved to csv.')
 
 
-def plot_stability_analysis_chart(percentage_stable: List[np.ndarray], noise_value: List[np.ndarray], names: List[str]):
+def plot_stability_analysis_chart(percentage_stable: List[np.ndarray], noise_value: List[np.ndarray], names: List[str],
+                                  envs: List[str]):
+    """
 
-    num_lines = len(percentage_stable)
+    Args:
+        percentage_stable: E x C x N. A list (length E) of numpy array of dimension (C x N), denoting the percentage stable for all controllers in all environments.
+        noise_value: 1 x N. A list (dimension N) of percentage noises.
+        names: 1 x C. A list (length C), denoting the names of the controllers.
+        envs: 1 x E. A list (length E) denoting the names of the environments.
+    Returns:
+        None
+
+    """
+    total_graphs = len(percentage_stable)
     colors = ['royalblue', 'orange', 'green']
-    # The x position of bars
 
     plt.style.use('seaborn-darkgrid')
 
-    # Create the bars
-    for i in range(num_lines):
-        plt.plot(noise_value[i], percentage_stable[i], color=colors[i % len(colors)], label=names[i])
-    # general layout
-    plt.xlabel('noise magnitude')
-    plt.ylabel('% stable')
-    plt.legend()
-    # Show graphic
+    fig, axs = plt.subplots(1, 3, figsize=(9, 3), constrained_layout=True)
+    # axs.xaxis.set_major_formatter(mtick.PercentFormatter(5.0))
+    # axs.yaxis.set_major_formatter(PercentFormatter())
+
+    for i in range(total_graphs):
+        cur_axis = axs[i]
+        x = noise_value
+        for j in range(len(percentage_stable[i])):
+            y = percentage_stable[i][j] * 100
+            cur_axis.plot(x, y, color=colors[j % len(colors)], label=names[j], linestyle='-', marker='o')
+        cur_axis.set_ylim(50, 105)
+        cur_axis.set_xlabel('Noise %')
+        cur_axis.set_ylabel('% trials stable')
+        cur_axis.legend()
+        cur_axis.set_title(envs[i])
+
     plt.show()
 
 
@@ -170,12 +196,12 @@ def plot_interaction_barchart(y_extended, y_pilco):
     plt.style.use('seaborn-darkgrid')
 
     # Create the bars
-    plt.bar(r1, y_pilco, width=barWidth, color='royalblue', capsize=15, label='PILCO')
+    plt.bar(r1, y_pilco, width=barWidth, color='royalblue', capsize=15, label='RBF Controller (PILCO)')
     plt.bar(r2, y_extended, width=barWidth, color='orange', capsize=15, label='Extended RBF controller')
 
     # general layout
     plt.xticks([r + barWidth / 2 for r in range(len(y_extended))], ['Swing-up pendulum', 'Cartpole', 'Mountain car'])
-    plt.ylabel('Interaction time')
+    plt.ylabel('Interaction time (s)')
     plt.legend()
 
     # Show graphic
@@ -183,8 +209,28 @@ def plot_interaction_barchart(y_extended, y_pilco):
 
 
 if __name__ == '__main__':
-    # Choose the height of the bars
-    y_extended = [18, 12.0, 6.2]
-    y_pilco = [26, 17.5, 9.6]
-
-    plot_interaction_barchart(y_extended, y_pilco)
+    # # Choose the height of the bars
+    # y_extended = [18, 12.0, 6.2]
+    # y_pilco = [26, 17.5, 9.6]
+    #
+    # plot_interaction_barchart(y_extended, y_pilco)
+    #
+    # noises = [5,10,20,30,50,70,100]
+    #
+    # swingup = np.asarray([[1,1,1,1,1,0.94,0.79],
+    #            [1,1,1,1,1,0.88,0.56],
+    #            [1,1,1,1,1,0.95,0.76]])
+    #
+    # cartpole = np.asarray([[1,1,1,1,1,1,1],
+    #             [1,1,1,1,1,1,0.85],
+    #             [1,1,1,1,1,0.98,0.99]])
+    #
+    # mountaincar = np.asarray([[1,1,1,1,1,1,0.88],
+    #                [1,1,1,1,0.93,0.8,0.71],
+    #                [1,1,1,1,1,1,0.91]])
+    #
+    # all = np.asarray([swingup, cartpole, mountaincar])
+    # names = ['Linear Controller', 'RBF Controller (PILCO)', 'Extended RBF Controller']
+    # envs = ['Swing-up pendulum', 'Cartpole', 'Mountain car']
+    # plot_stability_analysis_chart(all, noises, names, envs)
+    pass
