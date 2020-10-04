bc98db7549da8b2cda600fc2b12196441886b504
Nicholas Capel <nicholas.rj.capel@gmail.com>
2020-10-03 17:02:22 -0700

    Fix observation metric

--- dao/metrics.py
+++ dao/metrics.py
@@ -1,40 +1,38 @@
 import tf_agents.trajectories
-from tf_agents.metrics import tf_metric
 import gin
 import tensorflow as tf
-from tf_agents.metrics.tf_metrics import TFDeque
 from tf_agents.utils import common
 
 
 @gin.configurable(module='tf_agents')
-class CompleteStateObservation(tf_metric.TFStepMetric):
-  """Counts the number of steps taken in the environment."""
+class CompleteStateObservation:
+    """Counts the number of steps taken in the environment."""
 
-  def __init__(self, name='EnvironmentSteps', prefix='Metrics', dtype=tf.int64):
-    super(CompleteStateObservation, self).__init__(name=name, prefix=prefix)
-    self.dtype = dtype
-    self._all_trajectories = []
+    def __init__(self, env_spec=(0, 3), dtype=tf.float32):
+        self.dtype = dtype
+        self.env_spec = env_spec
+        self.dtype = dtype
+        self._storage = []
 
-  def call(self, trajectory: tf_agents.trajectories.trajectory.Trajectory):
-    """Increase the number of environment_steps according to trajectory.
+    def __call__(self, trajectory: tf_agents.trajectories.trajectory.Trajectory):
+        """Increase the number of environment_steps according to trajectory.
 
-    Step count is not increased on trajectory.boundary() since that step
-    is not part of any episode.
+        Step count is not increased on trajectory.boundary() since that step
+        is not part of any episode.
 
-    Args:
-      trajectory: A tf_agents.trajectory.Trajectory
+        Args:
+          trajectory: A tf_agents.trajectory.Trajectory
 
-    Returns:
-      The arguments, for easy chaining.
-    """
-    # The __call__ will execute this.
-    self._all_trajectories.append(trajectory)
-    return trajectory
+        Returns:
+          The arguments, for easy chaining.
+        """
+        # Zero out batch indices where a new episode is starting.
+        self._storage.append(trajectory.observation)
+        return trajectory
 
-  def result(self):
-    return self._all_trajectories
+    def result(self):
+        return self._storage
 
-  @common.function
-  def reset(self):
-    self._all_trajectories = []
-
+    @common.function
+    def reset(self):
+        self._storage = []
