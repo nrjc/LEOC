a67b312e39a6e8af01cccff9abd9a4936c122a6f
Funaizhang <naifuzhang@gmail.com>
2020-10-16 13:41:11 +0800

    set metas and clean up run.py

--- run.py
+++ run.py
@@ -1,34 +1,36 @@
+# Set metas for the entire experiment/run
 import numpy as np
 import gpflow
 gpflow.config.set_default_float(np.float64)
 
-from DDPG.ddpg import DDPG
-from DDPG.utils import train_ddpg
-from pilco.utils import train_pilco
+import tensorflow as tf
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+
+import gin
+
+# Import relevant local modules
+import dao.envs
 from dao.envloader import load_py_env
 from dao.trainer import PILCOTrainer, DDPGTrainer
-from pilco.controllers import LinearController
-import dao.envs
-import gin
 
 
 gin.parse_config_file('config.gin')
 # %%
-# ddpg_trainer = DDPGTrainer()
-# # %%
-# ddpg_trainer.train()
+ddpg_trainer = DDPGTrainer()
+# %%
+ddpg_trainer.train()
+#%%
+ddpg_trainer.save()
+#%%
+ddpg_trainer.load()
+#%%
+# pilco_trainer = PILCOTrainer()
 # #%%
-# ddpg_trainer.save()
+# pilco_trainer.train()
 # #%%
-# ddpg_trainer.load()
-#%%
-pilco_trainer = PILCOTrainer()
-#%%
-pilco_trainer.train()
-#%%
-pilco_trainer.save()
-#%%
-pilco_trainer.load()
-
-result = pilco_trainer.eval()
-print(result)
+# pilco_trainer.save()
+# #%%
+# pilco_trainer.load()
+#
+# result = pilco_trainer.eval()
+# print(result)
