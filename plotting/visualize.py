d37c5afeb6f672362671fb785ddaf45bc922575a
Funaizhang <naifuzhang@gmail.com>
2020-11-10 01:19:42 +0800

    Modify visualize.py

--- plotting/visualize.py
+++ plotting/visualize.py
@@ -1,7 +1,17 @@
-import gin
+import tensorflow as tf
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+from tf_agents.environments import suite_gym, tf_py_environment
 
+import dao.envs
 from plotting.visualiser import Visualiser
 
-gin.parse_config_file('../visualize_config.gin')
-visualizer = Visualiser()
+py_env = suite_gym.load('Pendulum-v7')
+model_path = '../controllers/pendulum/pilco_hybrid1'
+# py_env = suite_gym.load('Cartpole-v7')
+# model_path = '../controllers/cartpole/ddpg_hybrid1_-60'
+# py_env = suite_gym.load('Mountaincar-v7')
+# model_path = '../controllers/mountaincar/ddpg_hybrid2'
+env = tf_py_environment.TFPyEnvironment(py_env)
+
+visualizer = Visualiser(env, model_path)
 visualizer(500)
