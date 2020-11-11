631ea21d00748f8eda450466882408027916da94
Funaizhang <naifuzhang@gmail.com>
2020-11-11 10:53:52 +0800

    visualise mountaincar

--- plotting/visualize.py
+++ plotting/visualize.py
@@ -5,13 +5,13 @@ from tf_agents.environments import suite_gym, tf_py_environment
 import dao.envs
 from plotting.visualiser import Visualiser
 
-py_env = suite_gym.load('Pendulum-v7')
-model_path = '../controllers/pendulum/pilco_hybrid1'
+# py_env = suite_gym.load('Pendulum-v7')
+# model_path = '../controllers/pendulum/pilco_hybrid1'
 # py_env = suite_gym.load('Cartpole-v7')
 # model_path = '../controllers/cartpole/ddpg_hybrid1_-60'
-# py_env = suite_gym.load('Mountaincar-v7')
-# model_path = '../controllers/mountaincar/ddpg_hybrid2'
+py_env = suite_gym.load('Mountaincar-v7')
+model_path = '../controllers/mountaincar/pilco_baseline2'
 env = tf_py_environment.TFPyEnvironment(py_env)
 
 visualizer = Visualiser(env, model_path)
-visualizer(500)
+visualizer(1000)
