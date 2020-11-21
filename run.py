15c1827d422837882759bcbf517d7f8be74dfe27
Nicholas Capel <nicholas.rj.capel@gmail.com>
2020-11-21 11:27:12 -0800

    Adding changed run method

--- run.py
+++ run.py
@@ -1,31 +1,36 @@
 # Set metas for the entire experiment/run
-import numpy as np
+import argparse
+
 import gpflow
+import numpy as np
 
 gpflow.config.set_default_float(np.float64)
 
 import tensorflow as tf
+
 tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 
 import gin
 
-# Import relevant local modules
-import dao.envs
-from dao.envloader import load_py_env
 from dao.trainer import PILCOTrainer, DDPGTrainer
 
-
-gin.parse_config_file('config.gin')
-# %%
-# ddpg_trainer = DDPGTrainer()
-# # %%
-# ddpg_trainer.train()
-# #%%
-pilco_trainer = PILCOTrainer()
-#%%
-pilco_trainer.train()
-#%%
-
-
-# # Load and test control metrics
-# pilco_trainer = PILCOTrainer()
+parser = argparse.ArgumentParser(description='Run training from configuration file')
+parser.add_argument('-file', type=str, help='Location of .gin file containing training config', default='config.gin')
+args = parser.parse_args()
+gin.parse_config_file(args.file)
+try:
+    training_type = gin.query_parameter('type')
+except ValueError:
+    training_type = "ddpg"
+if training_type == "ddpg":
+    # %%
+    ddpg_trainer = DDPGTrainer()
+    # # %%
+    ddpg_trainer.train()
+elif training_type == "pilco":
+    # #%%
+    pilco_trainer = PILCOTrainer()
+    # %%
+    pilco_trainer.train()
+else:
+    raise NotImplementedError
