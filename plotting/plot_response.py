d8800a662b3597486909f29da004c31735be0e88
Funaizhang <naifuzhang@gmail.com>
2020-11-21 22:25:20 +0800

    Modify experiment for transient response

--- plotting/plot_response.py
+++ plotting/plot_response.py
@@ -3,18 +3,17 @@ from os import listdir
 from typing import List
 
 import tensorflow as tf
-from tf_agents.environments.py_environment import PyEnvironment
 from tf_agents.environments.tf_py_environment import TFPyEnvironment
 from tf_agents.environments import suite_gym, tf_py_environment
 
 import dao.envs
 from dao.trainer import Evaluator
-from plotting.plotter import ControlMetricsPlotter
+from plotting.plotter import TransientResponsePlotter
 
 tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 
 
-def get_metrics(envs_names: List[str], envs: List[TFPyEnvironment], policies: List[str],
+def get_response(envs_names: List[str], envs: List[TFPyEnvironment], policies: List[str],
                 impulse_input=0.0, step_input=0.0) -> dict:
     all_trajectories = {}  # a dict of dict containing trajectories for all envs
 
@@ -28,9 +27,10 @@ def get_metrics(envs_names: List[str], envs: List[TFPyEnvironment], policies: Li
             foldernames = [f for f in listdir(env_dir) if f.startswith(policy)]
             for model_folder in foldernames:
                 model_path = os.path.join(env_dir, model_folder)
-                myEvaluator = Evaluator(eval_env=envs[i], policy=None, plotter=None, model_path=model_path, eval_num_episodes=1)
+                myEvaluator = Evaluator(eval_env=envs[i], policy=None, plotter=None, model_path=model_path,
+                                        eval_num_episodes=1)
                 myEvaluator.load_policy()
-                trajectory = myEvaluator(training_timesteps=0, save_model=False,
+                trajectory = myEvaluator(training_time=0, save_model=False,
                                          impulse_input=impulse_input, step_input=step_input)
                 env_trajectories[policy].append(trajectory)
 
@@ -49,9 +49,9 @@ if __name__ == "__main__":
 
     envs = [pendulum_env, cartpole_env, mountaincar_env]
     envs_names = ['Pendulum', 'Cartpole', 'Mountaincar']
-    policies = ['ddpg_baseline0', 'ddpg_hybrid0', 'pilco_baseline0', 'pilco_hybrid0', 'linear']
+    policies = ['ddpg_baseline0', 'pilco_baseline0', 'linear']
 
-    impulse_trajectories = get_metrics(envs_names, envs, policies, impulse_input=-1.0)
-    step_trajectories = get_metrics(envs_names, envs, policies, step_input=-0.2)
-    myMetricsPlotter = ControlMetricsPlotter(envs_names)
+    impulse_trajectories = get_response(envs_names, envs, policies, impulse_input=-1.0)
+    step_trajectories = get_response(envs_names, envs, policies, step_input=-0.2)
+    myMetricsPlotter = TransientResponsePlotter(envs_names)
     myMetricsPlotter([impulse_trajectories, step_trajectories])
